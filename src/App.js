0a3a298e18e251d3c3ab30e3b7e3dda508f09359
git-Switchblade <dranzer662@gmail.com>
2022-11-11 07:46:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import React ,{ Suspense, useRef} from 'react';
+import { Section } from './components/section'
+// import { state } from './components/state.js'
 import { Canvas } from '@react-three/fiber';
-import { useCubeTexture, MeshDistortMaterial, Sphere, Html } from '@react-three/drei'
+import { useCubeTexture, MeshDistortMaterial, Sphere, Html,Text } from '@react-three/drei'
+import { createRef } from "react";
 
 function Scene() {
   const envMap = useCubeTexture(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'], { path: '/cube/' })
   return (
     <>
-      <Sphere args={[1.3,64,64]}>
+    <mesh>
+    <Sphere args={[1.3,64,64]}>
       <MeshDistortMaterial
         color={'#010101'}
         roughness={0.1}
@@ -19,20 +24,61 @@ function Scene() {
         envMap={envMap}
         />
         </Sphere>
+    </mesh>
+      
     </>
   )
 }
 
-function App() {
-  
+const HTMLContent = ({
+  domContent,
+  children,
+  bgColor,
+  modelPath,
+  position,
+}) => {
   return (
-    <Canvas>
-      <Scene />
+    <Section factor={1.5} offset={1}>
+      <group position={[0, position, 0]}>
+        <Html fullscreen portal={domContent}>
+          <div className='container'>
+            <h1 className='title'>{children}</h1>
+          </div>
+        </Html>
+      </group>
+    </Section>
+  );
+};
+
+function App() {
+  const states = {
+    sections: 5,
+    pages: 5,
+    zoom: 1,
+    top: createRef(),
+  };
+  const domContent = useRef()
+  return (
+    <>
+      <Canvas>
+      <Suspense>
+        <Scene />
+      </Suspense>
       <ambientLight intensity={1} />
-      <Html>
-        <h1 id="heading">Displacement</h1>
-      </Html>
+      <HTMLContent
+            domContent={domContent}
+            position={50}>
+            <span>And yes</span>
+            <span>we even got</span>
+            <span>monochrome!</span>
+          </HTMLContent>
     </Canvas>
+    <div className='scrollArea'>
+      <div style={{position: "sticky", top:0}} ref={domContent}></div>
+      <div style={{height: `${states.sections * 100}vh`}}></div>
+    </div>
+    </>
+    
   );
 }
 
